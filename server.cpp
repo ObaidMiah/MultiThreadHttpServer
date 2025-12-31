8df9e43caa78284f0cc14d16c209b02521d4af2e
obaid <obaidmiah@gmail.com>
2025-12-31 15:16:37 -0800

    Added HTTP request parsing and response routing

--- server.cpp
+++ server.cpp
@@ -4,10 +4,12 @@
 #include <string>
 #include <unistd.h>
 #include <thread>
+#include <sstream>
 
 // test
 #include <chrono>
 
+using std::istringstream;
 using std::string;
 using std::thread;
 
@@ -18,24 +20,74 @@ using std::this_thread::sleep_for;
 void handle_client(int client_connection)
 {
     // test
-    sleep_for(seconds(10));
+    // sleep_for(seconds(10));
 
     // read
     char buffer[4096];
-    int nRecv = recv(client_connection, buffer, sizeof(buffer), 0);
+    int bytes_received = recv(client_connection, buffer, sizeof(buffer), 0);
 
-    printf("%d bytes received from Client %d \n", nRecv, client_connection);
+    if (bytes_received <= 0)
+    {
+        close(client_connection);
+        return;
+    }
 
-    string response = "HTTP/1.1 200 OK\r\n"   // Protocol + status code + reason
-                      "Content-Length: 5\r\n" // bytes in body
-                      "\r\n"                  // end of headers
-                      "Hello";                // body
+    printf("%d bytes received from Client %d \n", bytes_received, client_connection);
 
-    ssize_t nSend = send(client_connection, response.c_str(), response.size(), 0);
+    // parse the http request
+
+    // GET /hello HTTP/1.1\r\n
+    // Host: localhost:8080\r\n
+    // User-Agent: curl/7.85.0\r\n
+    // Accept: */*\r\n
+    // Connection: close\r\n
+    // \r\n
+
+    string httpRequest(buffer, bytes_received);
+
+    size_t pos = httpRequest.find("\r\n");
+    string request_line = httpRequest.substr(0, pos);
+
+    istringstream iss(request_line);
+    string method, path, version;
+    iss >> method;
+    iss >> path;
+    iss >> version;
+
+    // print http request
+    // if (bytes_received > 0)
+    // {
+    //    printf("%.*s\n", bytes_received, buffer);
+    // }
+    // */
+
+    // construct the http response
+    string response_body;
+
+    if (path == "/")
+    {
+        response_body = "Welcome!";
+    }
+    else if (path == "/hello")
+    {
+        response_body = "Hello Word!";
+    }
+    else
+    {
+        response_body = "404 Not Found";
+    }
+
+    string httpResponse = "HTTP/1.1 200 OK\r\n" // Protocol + status code + reason
+                          "Content-Length: " +
+                          std::to_string(response_body.size()) + "\r\n" + // bytes in body
+                          "\r\n" +                                        // end of headers
+                          response_body;                                  // body
+
+    ssize_t nSend = send(client_connection, httpResponse.c_str(), httpResponse.size(), 0);
 
     if (nSend < 0)
     {
-        printf("Send failed to Client %d \n", client_connection);
+        printf("Failed to send to Client %d \n", client_connection);
     }
     else
     {
